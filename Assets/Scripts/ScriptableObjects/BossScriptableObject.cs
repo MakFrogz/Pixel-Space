14d6a6189a6ee8021600c3965aaa76189e9f8454
Mark Khrizman <badxpioneer2013@gmail.com>
2021-02-05 20:24:22 +0200

    added boss ScriptableObject

---
+++ Assets/Scripts/ScriptableObjects/BossScriptableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New boss data", menuName = "Boss/Create new boss data")]
+public class BossScriptableObject : ScriptableObject
+{
+    public float Damage;
+    public float Speed;
+    public float FireRate;
+    public float Health;
+}
